f4e55c7b41f0de484ee08c1fcf11776d16d6b909
LinusWeb <linooow@gmail.com>
2024-01-20 15:46:30 -0300

    Substituindo o X pelo *

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,86 +1,75 @@
+const display = document.getElementById('display_numeros');
+let expressao = '';
 
+const atualizarDisplay = () => {
+    display.value = expressao;
+}
 
+const adicionarAoDisplay = (valor) => {
+    expressao += valor;
+    atualizarDisplay();
+}    
 
+const limparDisplay = () => {
+    expressao = '';
+    atualizarDisplay();
+}
 
-const somar = () => {
-    if(primeiroNumero === "0") {
-      definirPrimeiroNumero(String(numeroAtual));
-      definirNumeroAtual("");
-      definirOperacao("+");
-    } else {
-      const sum = Number(primeiroNumero) + Number(numeroAtual)
-      definirNumeroAtual(String(sum));
-      definirOperacao("");
+const seletorDeOperacao = () => {
+  try {
+    expressao = math.evaluate(expressao.replace("x", "*")).toString();
+    atualizarDisplay();
+    } catch (error) {
+        console.error('Erro ao avaliar expressão:', error);
+        expressao = 'Erro';
+        atualizarDisplay();
     }
 }
 
-const limpar = () => {
-    definirNumeroAtual("0");
-    definirPrimeiroNumero("0");
-    definirOperacao("");
-}
-
-
-const subtrair = () => {
-    if(primeiroNumero === "0") {
-      definirPrimeiroNumero(String(numeroAtual));
-      definirNumeroAtual("");
-      definirOperacao("-");
-    } else {
-      const sum = Number(primeiroNumero) - Number(numeroAtual)
-      definirNumeroAtual(String(sum));
-      definirOperacao("");
+const calcularRaizQuadrada = () => {
+  try {
+    expressao = Math.sqrt(math.evaluate(expressao)).toString();
+    atualizarDisplay();
+    } catch (error) {
+        console.error('Erro ao calcular raiz quadrada:', error);
+        expressao = 'Erro';
+        atualizarDisplay();
     }
 }
 
-
-const multiplicar = () => {
-    if(primeiroNumero === "0") {
-      definirPrimeiroNumero(String(numeroAtual));
-      definirNumeroAtual("");
-      definirOperacao("x");
-    } else {
-      const sum = Number(primeiroNumero) * Number(numeroAtual)
-      definirNumeroAtual(String(sum));
-      definirOperacao("");
-    }
-}
-
-
-const dividir = () => {
-    if(primeiroNumero === "0") {
-      definirPrimeiroNumero(String(numeroAtual));
-      definirNumeroAtual("");
-      definirOperacao("/");
-    } else {
-      const sum = Number(primeiroNumero) / Number(numeroAtual)
-      definirNumeroAtual(String(sum));
-      definirOperacao("");
+const observarTeclado = (event) => { 
+  const teclaPressionada = event.key;
+    if (/^[0-9]+$/.test(teclaPressionada) || teclaPressionada === '.') {
+      adicionarAoDisplay(teclaPressionada);
+    } else if (/^[+\-*/]$/.test(teclaPressionada)) {
+        if (teclaPressionada === 'x'){
+            adicionarAoDisplay('*')
+        }else{
+            adicionarAoDisplay(teclaPressionada);
+        }
+    } else if (teclaPressionada === 'Enter') {
+        event.preventDefault ();
+        seletorDeOperacao();
     }
 }
 
 
-const totalizar = () => {
-    if(primeiroNumero !== "0" && operacao !== "" && numeroAtual !== "0") {
-      switch (operacao) {
-        case "+":
-          somar();
-          break;
-        
-        case "-":
-          subtrair();
-          break;
+document.addEventListener('keydown', observarTeclado);
 
-        case "x":
-          multiplicar();
-          break;  
-          
-        case "/":
-          dividir();
-          break;             
+const numeros = document.querySelectorAll('.numero');
 
-        default:
-          break;
-      }
-    }
-  }
+numeros.forEach(numero => {
+    numero.addEventListener('click', () => {
+        adicionarAoDisplay(numero.innerText);
+    });
+});
+
+document.getElementById('botao_limpar').addEventListener('click', limparDisplay);
+document.getElementById('botao_somar').addEventListener('click', () => adicionarAoDisplay('+'));
+document.getElementById('botao_potencia').addEventListener('click', () => adicionarAoDisplay('^'));
+document.getElementById('botao_subtrair').addEventListener('click', () => adicionarAoDisplay('-'));
+document.getElementById('botao_multiplicar').addEventListener('click', () => adicionarAoDisplay('x'));
+document.getElementById('botao_dividir').addEventListener('click', () => adicionarAoDisplay('/'));
+document.getElementById('botao_porcentagem').addEventListener('click', () => adicionarAoDisplay('%'));
+document.getElementById('botao_raiz').addEventListener('click', calcularRaizQuadrada);
+document.getElementById('igual').addEventListener('click', seletorDeOperacao);
